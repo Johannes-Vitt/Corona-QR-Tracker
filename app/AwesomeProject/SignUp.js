180290dd5a0563367dbca3305828ae33c80c99dc
Jonas Merforth <jonas.merofrth@gmx.de>
2020-03-21 20:30:22 +0100

    Linear gradient background, transparent logos

--- app/AwesomeProject/SignUp.js
+++ app/AwesomeProject/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { TextInput, Text, View, StyleSheet, TouchableOpacity, AsyncStorage, Alert, Image } from "react-native"
 import { Button } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
 
 const STORAGE_KEY = '@save_name'
 
@@ -22,8 +23,6 @@ export default class SignUp extends Component {
         if (this.state.email !== '' || this.state.tel !== '') {
             //TODO get the ID from the API
             this.props.createUser({ tel: this.state.tel, mail: this.state.email });
-            const id = '21313123';
-            this.props.save(id)
         } else {
             Alert.alert("Informationen unvollständig","Bitte E-Mail Adresse oder Telefonnummer eingeben!");
         }
@@ -31,17 +30,18 @@ export default class SignUp extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
+            <LinearGradient start={{x: 1, y: 0}} end={{x: 0, y: 1}} colors={['#76D7EC','#7AEA95', '#42EABE']} style={styles.linearGradient}>
                 <Image
                     style={{width: 150, height: 150}}
-                    source={require('./qr-icon.png')}
-                    //source={require('./Icon_full.png')}
+                    source={require('./ressources/qr-code-white-transparent-soft.png')}
+                    //source={require('./ressources/qr-icon.png')}
+                    //source={require('./ressources/Icon_full.png')}
                 />
-                <TextInput onChangeText={this.onChangeEmail} style={styles.input} placeholder='E-Mail Adresse'></TextInput>
-                <Text>ODER</Text>
-                <TextInput onChangeText={this.onChangeTel} style={styles.input} placeholder='Telefonnummer'></TextInput>
-                <Button title="Weiter" type="outline" onPress={this.proceed}></Button>
-            </View >);
+                <TextInput onChangeText={this.onChangeEmail} style={styles.input} placeholder='E-Mail Adresse' placeholderTextColor='white'></TextInput>
+                <Text style={styles.oder}>ODER</Text>
+                <TextInput onChangeText={this.onChangeTel} style={styles.input} placeholder='Telefonnummer' placeholderTextColor='white'></TextInput>
+                <Button color="#000000"title="Weiter" type="outline" onPress={this.proceed}></Button>
+            </LinearGradient> );
     };
 };
 
@@ -53,14 +53,24 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    oder:{
+        color: 'white'
+    },
     input: {
         fontSize: 30,
         marginTop: 50,
         marginBottom: 50,
+        color: 'white',
     },
     button: {
         fontSize: 50,
         color: '#000000',
         backgroundColor: '#147efb'
     },
+    linearGradient: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
+    }
 });
