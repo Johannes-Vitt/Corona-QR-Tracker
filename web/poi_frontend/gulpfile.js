3b25eb10d52c748816eb4085629e9a5123f7fd0f
Tobias Vitt <me@tobiasvitt.info>
2020-03-22 14:33:45 +0100

    test

--- web/poi_frontend/gulpfile.js
+++ web/poi_frontend/gulpfile.js
@@ -12,6 +12,7 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const serve = require('gulp-serve');
 
 // Load package.json for banner
 const pkg = require('./package.json');
@@ -129,6 +130,7 @@ function watchFiles() {
 const vendor = gulp.series(clean, modules);
 const build = gulp.series(vendor, gulp.parallel(css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const serving = gulp.series(build, serve('public'));
 
 // Export tasks
 exports.css = css;
@@ -138,3 +140,4 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+exports.serving = serving 
